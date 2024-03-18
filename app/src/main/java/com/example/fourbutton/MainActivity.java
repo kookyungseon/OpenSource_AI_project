97aa1b620fb23a476e563e40e2955b64d45e9c3b
kookyungseon <114800024+kookyungseon@users.noreply.github.com>
2024-03-18 16:26:41 +0900

    first_commit

---
+++ app/src/main/java/com/example/fourbutton/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.fourbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+
+    @Override
+   public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getApplicationContext(),"버튼을 눌렀어요",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        } );
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://m.naver.com"));
+                startActivity(intent);
+
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:010-1234-1234"));
+                startActivity(intent);
+
+            }
+        });
+//        button4.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // YouTube 앱으로 바로 이동하는 인텐트 설정
+//                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:"));
+//                // YouTube 앱이 없는 경우 Google Play Store로 이동
+//                if (intent.resolveActivity(getPackageManager()) != null) {
+//                    startActivity(intent);
+//                } else {
+//                    // Google Play Store 앱이 없는 경우 웹 브라우저로 열도록 설정
+//                    intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.google.android.youtube"));
+//                    startActivity(intent);
+//                }
+//            }
+//        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"));
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
